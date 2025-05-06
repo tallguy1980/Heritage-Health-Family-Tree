1b4688d4724144f79870e213905a16c7f4e08c84
DuJuan Pugh <dujuanpugh185@gmail.com>
2025-05-06 15:10:48 -0400

    Initial Commit

---
+++ Heritage Health Family Tree/Heritage_Health_Family_TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heritage_Health_Family_TreeApp.swift
+//  Heritage Health Family Tree
+//
+//  Created by DUJUAN PUGH on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Heritage_Health_Family_TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
